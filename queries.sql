1f395a2bea294485878639e0b50c07a7e1481d48
Feras Abdel-Rahim <abdelferas1@gmail.com>
2026-04-17 15:02:14 -0500

    Add query for sales and profit by category

--- queries.sql
+++ queries.sql
@@ -10,3 +10,15 @@ FROM superstore
 GROUP BY Region
 ORDER BY total_sales DESC;
 
+-- Sales and Profit by Category
+-- Business Question:
+-- Which product categories generate the most sales and profit?
+
+SELECT
+    Category,
+    ROUND(SUM(Sales), 2)  AS total_sales,
+    ROUND(SUM(Profit), 2) AS total_profit
+FROM superstore
+GROUP BY Category
+ORDER BY total_sales DESC;
+
